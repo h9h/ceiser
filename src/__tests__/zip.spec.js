543d8d53bb681f0ff2a1fd6eebe78339f6529bbe
Michael Heinke (SVI) <michael.heinke@sv-informatik.de>
2018-10-30 08:57:15 +0100

    Tests bereinigt

--- src/__tests__/zip.spec.js
+++ src/__tests__/zip.spec.js
@@ -3,15 +3,27 @@ import Logger from '../Logger'
 
 const log = Logger('zip.spec')
 
-it('should list zip-file entries', function () {
-  listZipEntries('./data/data.zip').then(zip => {
-    console.log(zip)
-    Object.keys(zip.files).forEach(key => console.log(key))
-  }).catch(error => log.error(error, 'Test failed'))
-})
+describe('handling von Zip-Datei', () => {
+  it('should list zip-file entries', function (done) {
+    listZipEntries('./data/data.zip').then(zip => {
+      expect(Object.keys(zip.files).length).toBe(27624)
+      done()
+    }).catch(error => {
+      log.error(error, 'Test failed')
+      done(error)
+    })
+  }, 2000)
 
-it('should read zip-file entries', function () {
-  readEntries('./data/data.zip', name => name === 'de/svi/xsd/beratung/candidate/ei/common/v1@ServiceNamespace.xceiser')
-    .then(contents => log.info({ contents }, 'Read contents'))
-    .catch(error => log.error(error, 'Test failed'))
+  it('should read zip-file entries', function (done) {
+    readEntries('./data/data.zip', name => name === 'de/svi/xsd/beratung/candidate/ei/common/v1@ServiceNamespace.xceiser')
+      .then(contents => {
+        log.info({ contents }, 'Read contents')
+        expect(contents.length).toBe(1)
+        done()
+      })
+      .catch(error => {
+        log.error(error, 'Test failed')
+        done(error)
+      })
+  }, 2000)
 })
