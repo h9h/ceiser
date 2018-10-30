97f6912b70925050f175dd590acf0a1363eafb6c
Michael Heinke (SVI) <michael.heinke@sv-informatik.de>
2018-10-30 08:15:19 +0100

    list zip-entries and read contents

--- src/__tests__/zip.spec.js
+++ src/__tests__/zip.spec.js
@@ -1,13 +1,17 @@
-import { readZip } from '../zip'
+import { listZipEntries, readEntries } from '../zip'
 import Logger from '../Logger'
 
 const log = Logger('zip.spec')
 
-it('should read zip-file', function () {
-  readZip('./data/data.zip').then(zip => {
+it('should list zip-file entries', function () {
+  listZipEntries('./data/data.zip').then(zip => {
     console.log(zip)
-    //Object.keys(zip.files).forEach(key => console.log(key))
-    zip.file('de/svi/xsd/beratung/candidate/ei/common/v1@ServiceNamespace.xceiser').async("string")
-      .then(text => console.log(text))
+    Object.keys(zip.files).forEach(key => console.log(key))
   }).catch(error => log.error(error, 'Test failed'))
+})
+
+it('should read zip-file entries', function () {
+  readEntries('./data/data.zip', name => name === 'de/svi/xsd/beratung/candidate/ei/common/v1@ServiceNamespace.xceiser')
+    .then(contents => log.info({ contents }, 'Read contents'))
+    .catch(error => log.error(error, 'Test failed'))
 })
--- src/zip.js
+++ src/zip.js
@@ -4,12 +4,33 @@ import Logger from './Logger'
 
 const log = Logger('zip')
 
-export const readZip = zipfile => new Promise((resolve, reject) => {
+export const listZipEntries = zipfile => new Promise((resolve, reject) => {
   fs.readFile(zipfile, (error, data) => {
     if (error) {
       log.error(error, 'Reading zip failed')
       reject(error)
     }
     JSZip.loadAsync(data).then(resolve)
+  })
+})
+
+export const filterConfigurationEvents = filename => !filename.match(/.*ConfigurationEvent\.xceiser$/)
+
+export const readEntries = (zipfile, filterFunction = filterConfigurationEvents) => new Promise((resolve, reject) => {
+  fs.readFile(zipfile, (error, data) => {
+    if (error) {
+      log.error(error, 'Reading zip failed')
+      reject(error)
+    }
+
+    JSZip.loadAsync(data).then(zip => {
+      const readEntries = Object.keys(zip.files).filter(filterFunction).map(filename => {
+        return zip.file(filename).async('string')
+      })
+
+      Promise.all(readEntries)
+        .then(resolve)
+        .catch(reject)
+    })
   })
 })
