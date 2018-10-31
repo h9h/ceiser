6087083bb3eb97a9ce7ef3b0dc9fcf36aca3726a
Michael (at Home) <dev@h9h.de>
2018-10-31 22:11:12 +0100

    CEISeR XML interpretiert zu Nodes, Relations und References

--- src/parseCEISeR.js
+++ src/parseCEISeR.js
@@ -3,27 +3,53 @@ import Logger from './Logger'
 
 const log = Logger('data-parseCEISeR')
 
-export const parseJSON = (json, parentFqn) => {
-  const label = getLabel(json)
-  const content = json[label]
+const createNode = (label, content, parentFqn) => {
   const namespace = content.namespace || parentFqn
   const fqn = getFqn(content, namespace)
+  const properties = getProperties(content)
+  const relatedBy = getRelations(content, parentFqn)
 
   const node = {
     label,
     type: getType(content),
     fqn,
-    properties: getProperties(content),
-    relatedTo: getRelations(content)
   }
 
-  log.trace({ json, node }, 'Finished parsing json, returning structure')
+  if (Object.keys(properties).length > 0) node.properties = properties
+  if (relatedBy.length > 0) node.relatedBy = relatedBy
+
+  log.trace({content, node}, 'Finished parsing json, returning structure')
   return node
+}
+
+const flatten = array => array.reduce(
+  (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []
+)
+
+export const parseJSON = (json, parentFqn) => {
+  if (parentFqn === '' || parentFqn == null) parentFqn = json[Object.keys(json)[0]].namespace
+
+  const label = getLabel(json)
+  if (label === undefined) {
+
+    return flatten(Object.keys(json).map(key => {
+      const sub = json[key]
+      if (Array.isArray(sub)) {
+        return sub.map(item => createNode(key, item, parentFqn))
+      } else {
+        return createNode(key, sub, parentFqn)
+      }
+    }))
+  } else {
+    const content = json[label]
+
+    return createNode(label, content, parentFqn)
+  }
 }
 
 const getLabel = json => {
   const keys = Object.keys(json)
-  if (keys.length !== 1) throwParseError('getLabel auf ungültigem JSON aufgerufen. Kein eindeutiger Type', json)
+  if (keys.length !== 1) return undefined
   return keys[0]
 }
 
@@ -43,16 +69,22 @@ const getFqn = (content, namespace) => {
 
 const getProperties = content => {
   const keys = Object.keys(content).filter(key => !['name', 'namespace', 'ID'].includes(key))
-  return Object.entries(content).filter(([key, value]) => keys.includes(key) && typeof value !== 'object')
+  const props = Object.entries(content).filter(([key, value]) => keys.includes(key) && typeof value !== 'object')
+  const result = {}
+  props.forEach(([key, value]) => result[key] = value)
+  return result
 }
 
 const getRelations = (content, parentFqn) => {
-  const entries = Object.entries(content).filter(([_, value]) => typeof value === 'object' && Object.keys(value).length === 1)
+  const entries = Object.entries(content).filter(([_, value]) => typeof value === 'object')
+
   return entries.map(([key, value]) => {
+    const relatedTo = parseJSON(value, parentFqn)
+
     return {
       label: key,
       type: 'Relation',
-      relatedTo: (Array.isArray(value) ? value : [value]).map(v => parseJSON(v, parentFqn))
+      relatedTo
     }
   })
 }
